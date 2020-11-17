b51fac3997317cb9fb84aa73543705cfc3c40bb4
Andrey Chubarovskiy <52russiadk@gmail.com>
2020-11-17 23:18:21 +0300

    Improved mendeleev_json

--- mendeleev_json/mendeleev_json.rb
+++ mendeleev_json/mendeleev_json.rb
@@ -13,12 +13,18 @@ file = File.read('mendeleev.json', encoding: 'UTF-8')
 elements = JSON.parse(file)
 
 puts "У нас есть #{elements.keys.size} элементов"
-puts elements.keys
+puts elements.keys.to_s
 
+puts
+puts 'О каком элементе хотите узнать?'
 element = gets.chomp
 
 if elements.key?(element)
-  puts "Первооткрыватель элемента: #{elements[element]}"
+  choice = elements[element]
+  puts "Порядковый номер: #{choice['number']}"
+  puts "Название: #{choice['name']}"
+  puts "Первооткрыватель: #{choice['discoverer']} (#{choice['year']})"
+  puts "Плотность: #{choice['density']} г/см³"
 else
   puts 'Такой элемент ещё не открыт'
 end
