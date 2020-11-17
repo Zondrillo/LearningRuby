0a7e1bac5a78bc62396cf4b7f0f1d07ff554a008
Andrey Chubarovskiy <52russiadk@gmail.com>
2020-11-17 10:54:47 +0300

    Improved business card xml to html converter

--- business_card_converter/converter.rb
+++ business_card_converter/converter.rb
@@ -4,14 +4,15 @@ html_file_path = './data/my_business_card.html'
 xml_file_path = './data/my_business_card.xml'
 img_path = '../images/Stewie_Griffin.png'
 
-unless File.exist?(html_file_path)
-  File.open(html_file_path, 'w:UTF-8') do |file|
-    file.puts '<!DOCTYPE html>'
-    file.puts '<html lang="en"></html>'
-  end
+begin
+  xml_file = File.open(xml_file_path, 'r:UTF-8')
+rescue Errno::ENOENT => e
+  regexp = %r{/data/(.*)}i
+  file_name = xml_file_path.scan(regexp)
+  puts "XML file #{file_name} not found"
+  abort e.message
 end
 
-xml_file = File.open(xml_file_path, 'r:UTF-8')
 xml = REXML::Document.new(xml_file)
 xml_file.close
 
@@ -20,25 +21,26 @@ xml.root.elements.each do |item|
   xml_data[item.name] = item.text
 end
 
+unless File.exist?(html_file_path)
+  File.open(html_file_path, 'w:UTF-8') do |file|
+    file.puts '<!DOCTYPE html>'
+  end
+end
+
 html_file = File.open(html_file_path, 'r:UTF-8')
 html = REXML::Document.new(html_file)
 html_file.close
 
-html_tag = html.elements.find('html').first
-head = html_tag.add_element('head')
+html.add_element('html', {'lang' => 'en'})
+head = html.root.add_element('head')
 head.add_element('meta', {'charset' => 'UTF-8'})
-title = head.add_element('title')
-title.text = "#{xml_data['first_name']} #{xml_data['last_name']}"
-body = html_tag.add_element('body')
-body.add_element('img', {'src' => img_path})
-name = body.add_element('p')
-name.text = title.text
-description = body.add_element('p')
-description.text = xml_data['description']
-phone = body.add_element('p')
-phone.text = "Phone: #{xml_data['phone']}"
-email = body.add_element('p')
-email.text = "Email: #{xml_data['email']}"
+title = head.add_element('title').add_text("#{xml_data['first_name']} #{xml_data['last_name']}")
+body = html.root.add_element('body')
+body.add_element('p').add_element('img', {'src' => img_path}) # image
+body.add_element('p').add_text(title.text) # name
+body.add_element('p').add_text(xml_data['description']) # description
+body.add_element('p').add_text("Phone: #{xml_data['phone']}") # phone
+body.add_element('p').add_text("Email: #{xml_data['email']}") # email
 
 File.open(html_file_path, 'w:UTF-8') do |html_file|
   html.write(html_file, 4)
